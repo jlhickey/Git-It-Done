c84fb26efd37ca00997f3f2453364f85269c2d4a
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-06-23 10:40:22 -0400

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,35 @@
+
+
+
+var getUserRepos = function () {
+    fetch("https://api.github.com/users/octocat/repos");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="./homepage.js></script></body>
+</html>
